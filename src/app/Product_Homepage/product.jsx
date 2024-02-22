8a3f10f1307d961564d4770cb67077eefa975438
Aman Dangol <icrextha@gmail.com>
2024-02-22 23:40:21 +0545

    deployment fetching fixed

--- src/app/Product_Homepage/product.jsx
+++ src/app/Product_Homepage/product.jsx
@@ -13,7 +13,6 @@ import './ProductHome.css';
 import CircularIndicator from '../utils/CircularIndicator'; 
 import Footer from '../components/footer';
 
-
 const ProductHomepage = () => {
   const Router = useRouter();
   const { profile } = useContext(UserContext);
@@ -24,16 +23,12 @@ const ProductHomepage = () => {
   const [loading, setLoading] = useState(true);
   const [quantityMap, setQuantityMap] = useState({}); // State to manage quantity for each product
   const fakeStoreApi = process.env.NEXT_PUBLIC_FAKE_STORE_API;
-  
-
   let arrayPrice = [];
 
   let userCountry = profile?.country || '';
   let countryCode = getCode(userCountry);
   let countryCurrency = countryToCurrency[countryCode];
   let currencySymbol = getSymbolFromCurrency(countryCurrency) || '$';
-
-  
 
   const style = {
     position: 'absolute',
@@ -59,7 +54,7 @@ const ProductHomepage = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://api.escuelajs.co/api/v1/products");
+      const response = await fetch(fakeStoreApi);
       const contentType = response.headers.get('content-type');
   
       if (!response.ok) {
@@ -71,12 +66,16 @@ const ProductHomepage = () => {
       }
   
       const data = await response.json();
-      setProducts(data);
+  
+      // Limit the number of products to the first 16
+      const slicedData = data.slice(0, 16);
+  
+      setProducts(slicedData);
       setLoading(false);
   
       // Initialize quantity state for each product to 1
       const initialQuantityMap = {};
-      data.forEach(product => {
+      slicedData.forEach(product => {
         initialQuantityMap[product.id] = 0;
       });
       setQuantityMap(initialQuantityMap);
@@ -86,9 +85,6 @@ const ProductHomepage = () => {
     }
   };
   
-  
-  
-
   const checkItems = (product, event) => {
     if (profile === null || profile === undefined) {
         alert("You must login first!");
@@ -98,19 +94,7 @@ const ProductHomepage = () => {
         addToCart(product, event, arrayPrice, quantity); // Make sure quantity is passed here
         setOpen(true);
     }
-};
-
-const categories = {};
-
-// Categorize products based on their category
-products.forEach((product) => {
-  if (!categories[product.category]) {
-    categories[product.category] = [];
-  }
-  categories[product.category].push(product);
-});
-
-
+  };
 
   const newPrice = () => {
     try {
@@ -129,18 +113,8 @@ products.forEach((product) => {
     }
   };
 
-  
-
   const noDiscountedPrice = (price) => {
     let currentPrice = (Math.round(price * newPrice()));
-
-    arrayPrice.push(currentPrice);
-    return currentPrice;
-  };
-
-  const updatedPrice = (price) => {
-    let currentPrice = (Math.round(price * newPrice()));
-
     arrayPrice.push(currentPrice);
     return currentPrice;
   };
@@ -159,35 +133,22 @@ products.forEach((product) => {
       ...prevState,
       [productId]: prevState[productId] + 1
     }));
-    console.log('Quantity Map after increment:', quantityMap);
   };
+
   const decrementQuantity = (productId) => {
     if (quantityMap[productId] > 1) {
         setQuantityMap(prevState => ({
             ...prevState,
             [productId]: prevState[productId] - 1
         }));
-        console.log('Quantity Map after decrement:', quantityMap);
     } else if (quantityMap[productId] === 1) {
         setQuantityMap(prevState => ({
             ...prevState,
             [productId]: 0
         }));
     }
-};
+  };
 
-  // Inside the ProductHome component
-
-const addToCartHandler = (product) => {
-  
-  const quantity = quantityMap[product.id];
-  // Clear the arrayPrice before adding new prices
-  arrayPrice = [];
-  // Call the addToCart function from context
-  addToCart(product, event, price, quantity);
-};
-
-  
   useEffect(() => { 
     getRates();
     fetchProducts();
@@ -196,43 +157,34 @@ const addToCartHandler = (product) => {
   if (loading) {
     return <CircularIndicator />;
   }
-  
-  
 
   return (
     <div className="ProductHome">
-      <div className="categories-container">
-        {Object.entries(categories).map(([category, productsInCategory]) => (
-          <div key={category} className="category">
-            <h2 className="category-title">{category.toUpperCase()}</h2>
-            <div className="products-container grid grid-cols-2 gap-4">
-              {productsInCategory.map((product) => (
-                <div key={product.id} className="ProductItem rounded-xl backdrop-blur-md bg-blue/50 p-2 hover:drop-shadow-xl hover:bg-slate-50">
-                  <img src={product.image} alt={product.title} />
-                  <div>
-                    <div className="font-bold text-lg pb-3">{product.title}</div>
-                    <div className="font-semi text-md pb-3">{product.description}</div>
-                    <div className="py-5">
-                      <span className="price">{currencySymbol}</span>
-                      <span className="price-value">
-                        {Intl.NumberFormat().format(Math.round(noDiscountedPrice(product.price)))}
-                      </span>
-                    </div>
-                    <div className="quantity-controls pt-2">
-                      <button className="quantity-button" onClick={() => decrementQuantity(product.id)}>
-                        <FontAwesomeIcon icon={faMinus} />
-                      </button>
-                      <span className="quantity-display">{quantityMap[product.id]}</span>
-                      <button className="quantity-button" onClick={() => incrementQuantity(product.id)}>
-                        <FontAwesomeIcon icon={faPlus} />
-                      </button>
-                    </div>
-                    <form className="pt-5" onSubmit={(event) => { event.preventDefault(); checkItems(product, event, quantityMap[product.id]); }}>
-                      <button className={`mx-2 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 ${quantityMap[product.id] === 0 && 'disabled'}`} disabled={quantityMap[product.id] === 0}>Add to Cart</button>
-                    </form>
-                  </div>
-                </div>
-              ))}
+      <div className="products-container grid grid-cols-2 gap-4">
+        {products.map(product => (
+          <div key={product.id} className="ProductItem rounded-xl backdrop-blur-md bg-blue/50 p-2 hover:drop-shadow-xl hover:bg-slate-50">
+            <img src={product.images[0]} alt={product.title} />
+            <div>
+              <div className="font-bold text-lg pb-3">{product.title}</div>
+              <div className="font-semi text-md pb-3">{product.description}</div>
+              <div className="py-5">
+                <span className="price">{currencySymbol}</span>
+                <span className="price-value">
+                  {Intl.NumberFormat().format(Math.round(noDiscountedPrice(product.price)))}
+                </span>
+              </div>
+              <div className="quantity-controls pt-2">
+                <button className="quantity-button" onClick={() => decrementQuantity(product.id)}>
+                  <FontAwesomeIcon icon={faMinus} />
+                </button>
+                <span className="quantity-display">{quantityMap[product.id]}</span>
+                <button className="quantity-button" onClick={() => incrementQuantity(product.id)}>
+                  <FontAwesomeIcon icon={faPlus} />
+                </button>
+              </div>
+              <form className="pt-5" onSubmit={(event) => { event.preventDefault(); checkItems(product, event, quantityMap[product.id]); }}>
+                <button className={`mx-2 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 ${quantityMap[product.id] === 0 && 'disabled'}`} disabled={quantityMap[product.id] === 0}>Add to Cart</button>
+              </form>
             </div>
           </div>
         ))}
@@ -254,10 +206,8 @@ const addToCartHandler = (product) => {
         </Box>
       </Modal>
       <Footer />
-
     </div>
   );
-
-  
 };
+
 export default ProductHomepage;
